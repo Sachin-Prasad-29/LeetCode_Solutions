ddabb7375edd8aecb698a76a8f3284258f57a18a
Sachin kumar prasad <39449370+Sachin-Prasad-29@users.noreply.github.com>
2022-05-20 01:58:16 +0530

    Time: 5 ms (92.47%), Space: 42 MB (68.20%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,20 +1,20 @@
 class Solution {
-    public int findTargetSumWays(int[] nums, int target) {
-        int R= dfs(1,nums,target,nums[0]);
-        int L = dfs(1, nums,target,-nums[0]);
-        return R+L;
-    }
-    
-    private static int dfs(int index, int[] nums, int target, int sum){
-        if(index == nums.length){
-            if(sum == target)
-                return 1;
-            else 
-                return 0;
+    public int findTargetSumWays(int[] nums, int s) {
+        int sum = 0; 
+        for(int i: nums) sum+=i;
+        if(s>sum || s<-sum) return 0;
+        int[] dp = new int[2*sum+1];
+        dp[0+sum] = 1;
+        for(int i = 0; i<nums.length; i++){
+            int[] next = new int[2*sum+1];
+            for(int k = 0; k<2*sum+1; k++){
+                if(dp[k]!=0){
+                    next[k + nums[i]] += dp[k];
+                    next[k - nums[i]] += dp[k];
+                }
+            }
+            dp = next;
         }
-        int R = dfs(index+1, nums, target, sum+nums[index]);
-        int L = dfs(index+1, nums, target, sum-nums[index]);
-        
-        return R+L;
+        return dp[sum+s];
     }
 }
