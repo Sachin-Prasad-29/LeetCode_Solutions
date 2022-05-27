d3a944c1b6913b845fb0626e9a193c58c8319d23
Sachin kumar prasad <39449370+Sachin-Prasad-29@users.noreply.github.com>
2022-05-27 16:29:52 +0530

    Time: 4 ms (14.84%), Space: 44.8 MB (5.29%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.java
+++ 392-is-subsequence/392-is-subsequence.java
@@ -3,17 +3,26 @@ class Solution {
         int M = s.length();
         int N = t.length();
         
-        return helper(s,t,M,N);
+        int[][] dp = new int[M+1][N+1];
+        for(int[] d : dp)
+            Arrays.fill(d,-1);
+        
+        if(M ==helper(s,t,M,N,dp)) return true;
+        return false;
     }
-    private static boolean helper(String s, String t, int i , int j){
+    private static int helper(String s, String t, int i , int j,int[][] dp){
         if(i == 0 || j == 0){
-            if(i == 0) return true;
-            return false;
+            return 0;
         }
+        if(dp[i][j] != -1) return dp[i][j];
         if(s.charAt(i-1) == t.charAt(j-1)){
-            return helper(s,t,i-1,j-1);
+            dp[i][j] =1+helper(s,t,i-1,j-1,dp);
+            return dp[i][j];
         }
-        else 
-            return helper(s,t,i,j-1);
+        else {
+            dp[i][j] = helper(s,t,i,j-1,dp);
+            return dp[i][j];
+        }
+            
     }
 }
