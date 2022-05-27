22cde9ddd93c0136f34d06d465a5989d3bd72939
Sachin kumar prasad <39449370+Sachin-Prasad-29@users.noreply.github.com>
2022-05-27 09:15:03 +0530

    Time: 64 ms (7.54%), Space: 49.9 MB (24.87%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -1,54 +1,58 @@
 class Solution {
-    public String shortestCommonSupersequence(String str1, String str2) {
-        int m = str1.length();
-        int n = str2.length();
-
-        int[][] dp = new int[m + 1][n + 1];
-
-        for (int i = 0; i <= m; i++)
-        {
-            for (int j = 0; j <= n; j++)
-            {
-                if (i == 0)
-                    dp[i][j] = j;
-                else if (j == 0)
-                    dp[i][j] = i;
-                else if (str1.charAt(i - 1) == str2.charAt(j - 1))
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+    public String shortestCommonSupersequence(String s1, String s2) {
+        int M = s1.length();
+        int N = s2.length();
+        
+        int[][] t = new int[M+1][N+1];
+        
+        for(int i = 0; i< M+1; i++){
+            for(int j = 0 ; j < N+1 ; j++){
+                if(i == 0)
+                    t[i][j] = j;
+                else if( j == 0)
+                    t[i][j] = i;
+                else if(s1.charAt(i-1) == s2.charAt(j-1))
+                    t[i][j] = 1+ t[i-1][j-1];
                 else
-                    dp[i][j] = 1 + Math.min(dp[i - 1][j],
-                            dp[i][j - 1]);
+                    t[i][j] = 1+ Math.min(t[i-1][j], t[i][j-1]);
             }
         }
-
-        int l = dp[m][n]; // Length of the ShortestSuperSequence
-        char[] arr = new char[l];
-        int i=m, j=n;
-        while(i>0 && j>0)
-        {
-            /* If current character in str1 and str2 are same, then
-             current character is part of shortest supersequence */
-            if(str1.charAt(i-1) == str2.charAt(j-1)) {
-                arr[--l] = str1.charAt(i-1);
-                i--;j--;
-            }else if(dp[i-1][j]<dp[i][j-1]) {
-                arr[--l] = str1.charAt(i-1);
+        
+        int i = M;
+        int j = N;
+        StringBuilder sb = new StringBuilder();
+        while(i > 0 && j > 0){
+            System.out.println(i+" "+j+ " "+ sb.toString());
+            if(s1.charAt(i-1) == s2.charAt(j-1)){
+                sb.append(s1.charAt(i-1));
+                i--;
+                j--;
+            }
+            else{
+                if(t[i-1][j] < t[i][j-1]){
+                    sb.append(s1.charAt(i-1));
+                    i--;
+                }
+                else{
+                    sb.append(s2.charAt(j-1));
+                    j--;
+                }
+            }
+        }
+        if(i > 0){
+            while(i > 0){
+                sb.append(s1.charAt(i-1));
                 i--;
             }
-            else {
-                arr[--l] = str2.charAt(j-1);
+        }
+        if(j > 0){
+            while(j > 0){
+                sb.append(s2.charAt(j-1));
                 j--;
             }
         }
-        while (i > 0) {
-            arr[--l] = str1.charAt(i-1);
-            i--;
-        }
-        while (j > 0) {
-            arr[--l] = str2.charAt(j-1);
-            j--;
-        }
-        return new String(arr);
+        System.out.println(sb.toString());
+        return sb.reverse().toString();
     }
 }
 
