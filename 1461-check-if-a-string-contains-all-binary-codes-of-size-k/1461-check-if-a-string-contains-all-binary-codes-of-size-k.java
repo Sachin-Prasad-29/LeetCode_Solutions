8253ef65b024ee0b13d514b9d98a0818ea30a66a
Sachin kumar prasad <39449370+Sachin-Prasad-29@users.noreply.github.com>
2022-05-31 16:43:30 +0530

    Time: 149 ms (59.38%), Space: 97.4 MB (38.02%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -1,25 +1,32 @@
 class Solution {
     public boolean hasAllCodes(String s, int k) {
         int N = s.length();
+        // Storing the maximum possible combination with k digit 
         int combi = (int)Math.pow(2,k);
+        
+        // initilaizing the starting and End pointer
         int start=0, end = 0;
+        
         HashSet<String> set = new HashSet<>();
         
         while(end <  N){
+            // untill we reach the window size of k increase the end value only
             if(end - start +1 < k){
                 end++;
                 continue;
             }
+            // if size of the window is equal to the k 
             else if( end - start+1 == k){
-                
-                String temp = s.substring(start,end+1);
+                 String temp = s.substring(start,end+1);
+                // insert the substring of size k in the set 
                 set.add(temp);
+                // check wheather the size of the set is equal to the requied combination or not 
                 if(set.size() == combi) return true;
                 start++;
                 end++;
             }
         }
-        if(set.size() == combi) return true;
+        
         return false;
     }
 }
