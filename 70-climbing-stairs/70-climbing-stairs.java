db33ab809c5ffd84bb555a91672143f7187a0ac0
Sachin kumar prasad <39449370+Sachin-Prasad-29@users.noreply.github.com>
2022-06-03 00:48:18 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (69.60%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,14 +1,22 @@
 class Solution {
+    // Array to memoize the solution 
     static int[] dp ;
     public int climbStairs(int n) {
+        // creating ans initilizing the array 
         dp = new int[n+1];
         Arrays.fill(dp,-1);
+        // calling the helper function
         return helper(n);
     }
     private static int helper(int n){
+        // base conditions
         if(n == 0 ) return 1;
-        if(n < 0) return 0;
+        if(n == 1) return 1;
+        
+        // check if we already solved it previously 
         if(dp[n] != -1) return dp[n];
+        
+        // store the answer in dp array to avoid it solving again and again
         return dp[n] = helper(n-1) + helper(n-2);
         
     }
