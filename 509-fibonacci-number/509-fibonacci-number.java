92269c8e6f542a17b36618a3508e1a974b763e51
Sachin kumar prasad <39449370+Sachin-Prasad-29@users.noreply.github.com>
2022-06-04 18:13:26 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (45.28%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fib(int n) {
+        if( n == 0 ) return 0;
+        int prev = 1;
+        int prePrev = 0 ; 
+        for(int i = 2 ; i <= n ; i++){
+            int curr = prev + prePrev;
+            prePrev = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+}
