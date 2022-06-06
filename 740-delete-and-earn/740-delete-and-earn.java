55dd673dca26c22d10cc8d084abbd16842b4066e
Sachin kumar prasad <39449370+Sachin-Prasad-29@users.noreply.github.com>
2022-06-06 15:27:49 +0530

    Time: 10 ms (60.16%), Space: 48.9 MB (25.24%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.java
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -13,7 +13,7 @@ class Solution {
         }
         dp = new int[10002];
         Arrays.fill(dp,-1);
-        System.out.println(Arrays.toString(sum));
+       // System.out.println(Arrays.toString(sum));
         
         
         return Math.max(helper(sum,10000),helper(sum,9999));
