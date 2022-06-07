f8fab550bd860964c8409975e32ed61721b7310f
Sachin kumar prasad <39449370+Sachin-Prasad-29@users.noreply.github.com>
2022-06-07 18:53:16 +0530

    Time: 2 ms (88.16%), Space: 65.8 MB (72.00%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,18 +1,26 @@
 class Solution {
-    static int[] dp;
-    public boolean canJump(int[] nums) {
-        dp = new int[nums.length];
-        Arrays.fill(dp,-1);
-        return helper(0, nums) == 1;
-    }
-    private static int helper(int i , int[] nums){
-        if(i == nums.length-1){
-                return 1;
-        }
-        if(dp[i] != -1) return dp[i];
-        for(int k = 1; k <= nums[i];k++){
-            if(helper(i+k,nums) == 1) return dp[i] = 1;
-        }
-        return dp[i] = 0 ;
-    }
+     public boolean canJump(int[] nums) {
+         int goal = nums.length-1;
+         for(int i = nums.length-2; i >= 0; i--){
+            if(nums[i]+ i  >= goal) goal = i;
+         }
+         return goal == 0;
+     }
 }
+//     static int[] dp;
+//     public boolean canJump(int[] nums) {
+//         dp = new int[nums.length];
+//         Arrays.fill(dp,-1);
+//         return helper(0, nums) == 1;
+//     }
+//     private static int helper(int i , int[] nums){
+//         if(i == nums.length-1){
+//                 return 1;
+//         }
+//         if(dp[i] != -1) return dp[i];
+//         for(int k = 1; k <= nums[i];k++){
+//             if(helper(i+k,nums) == 1) return dp[i] = 1;
+//         }
+//         return dp[i] = 0 ;
+//     }
+// }
