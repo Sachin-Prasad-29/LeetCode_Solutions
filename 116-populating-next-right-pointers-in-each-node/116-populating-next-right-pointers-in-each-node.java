ab8073ad3afe59fac4103a7ceca6ee03b6fe2260
Sachin kumar prasad <39449370+Sachin-Prasad-29@users.noreply.github.com>
2022-06-12 02:21:22 +0530

    Time: 0 ms (100.00%), Space: 47.7 MB (27.08%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.java
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.java
@@ -21,33 +21,56 @@ class Node {
 };
 */
 
+
+
 class Solution {
     public Node connect(Node root) {
-        if(root == null) return root;
-        Queue<Node> Q1 = new LinkedList<>();
-        Queue<Node> Q2 = new LinkedList<>();
+        if(root == null ) return root;
+        Node curr = root;
+        Node nxt = root.left;
         
-        Q1.offer(root);
-        
-        while(!Q1.isEmpty() || !Q2.isEmpty()){
-            if(Q1.isEmpty())
-                bfs(Q2,Q1);
-            else
-                bfs(Q1,Q2);
+        while(nxt != null && curr != null){
+            curr.left.next = curr.right;
+            if(curr.next != null){
+                curr.right.next = curr.next.left;
+                curr = curr.next;
+            }
+            else{
+                curr = nxt;
+                nxt = curr.left;
+            }
         }
         return root;
     }
-    private static void bfs(Queue<Node> Qs, Queue<Node> Qd){
-        while(!Qs.isEmpty()){
-            Node n = Qs.poll();
-            if(!Qs.isEmpty()) n.next = Qs.peek();
-            else n.next = null;
-            
-            if(n.left != null)Qd.offer(n.left);
-            if(n.right != null) Qd.offer(n.right);
-        }
-    }
-    
 }
+
+// class Solution {
+//     public Node connect(Node root) {
+//         if(root == null) return root;
+//         Queue<Node> Q1 = new LinkedList<>();
+//         Queue<Node> Q2 = new LinkedList<>();
+        
+//         Q1.offer(root);
+        
+//         while(!Q1.isEmpty() || !Q2.isEmpty()){
+//             if(Q1.isEmpty())
+//                 bfs(Q2,Q1);
+//             else
+//                 bfs(Q1,Q2);
+//         }
+//         return root;
+//     }
+//     private static void bfs(Queue<Node> Qs, Queue<Node> Qd){
+//         while(!Qs.isEmpty()){
+//             Node n = Qs.poll();
+//             if(!Qs.isEmpty()) n.next = Qs.peek();
+//             else n.next = null;
+            
+//             if(n.left != null)Qd.offer(n.left);
+//             if(n.right != null) Qd.offer(n.right);
+//         }
+//     }
+    
+// }
 
 
