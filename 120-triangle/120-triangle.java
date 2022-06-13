f584690d6454aee1e19cf302a68a1d29238d3c8e
Sachin kumar prasad <39449370+Sachin-Prasad-29@users.noreply.github.com>
2022-06-14 00:51:49 +0530

    Time: 4 ms (61.51%), Space: 44.5 MB (24.79%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,16 +1,44 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int m = triangle.size();
-        int[][] dp = new int[m+1][m+1];
+        int[][] dp = new int[m][m];
+        // initialize the dp array
+        for(int j = m-1; j>=0;j--)
+            dp[m-1][j] = triangle.get(m-1).get(j);
+        
+        // iterative for loops
+        
+        for(int i = m-2; i >= 0 ;i--){
+            for(int j = i; j >= 0 ; j--){
+                int down = triangle.get(i).get(j) + dp[i+1][j];
+                int dia = triangle.get(i).get(j) + dp[i+1][j+1];
+                dp[i][j]= Math.min(down,dia);
+            }
+        }
+         return dp[0][0];
+        
+    }
+
+    
+    
+    
+    
+    public int minimugfgmTotal(List<List<Integer>> triangle) {
+        int m = triangle.size();
+        int[][] dp = new int[m][m];
         for(int[] d:dp) Arrays.fill(d,-1);
        return solve(0,0,triangle,dp);
     }
     private static int solve(int m, int n , List<List<Integer>> tri,int[][] dp){
+        
         if(m == tri.size()-1)
             return tri.get(m).get(n);
+        
         if(dp[m][n] != -1) return dp[m][n];
+        
         int down = tri.get(m).get(n) + solve(m+1,n,tri,dp);
         int dia = tri.get(m).get(n) + solve(m+1,n+1,tri,dp);
+        
         return dp[m][n]=Math.min(down,dia);
     }
 }
