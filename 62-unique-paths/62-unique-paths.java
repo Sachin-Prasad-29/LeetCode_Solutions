eca4faf9932be04f43e647b51c479a5f66ca22bb
Sachin kumar prasad <39449370+Sachin-Prasad-29@users.noreply.github.com>
2022-06-13 22:50:44 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (96.43%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,6 +1,25 @@
 class Solution {
     // Buttom up (tabuluation approach)
      public int uniquePaths(int m, int n) {
+         int[] dp = new int[m];
+         int[] temp = new int[n];
+         for(int i= 0; i < m; i++ ){
+             for(int j = 0; j < n; j++ ){
+                 if(i == 0 && j == 0) temp[j]= 1;
+                 else{
+                     int left = 0, up = 0;
+                     if(j > 0) left = temp[j-1];
+                     if(i > 0) up  = dp[j];
+                     temp[j] = up+left;
+                 }
+              }
+             dp = temp;
+         }
+         
+         return temp[n-1];
+     }
+    // Buttom up (tabuluation approach)
+     public int uniquePatsdhs(int m, int n) {
          int[][] dp = new int[m][n];
          dp[0][0] = 1;
          for(int i= 0; i < m; i++ )
