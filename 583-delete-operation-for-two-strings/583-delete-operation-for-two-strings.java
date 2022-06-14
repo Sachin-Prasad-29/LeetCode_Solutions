24850a80813a417976ffcf6cc690a011d30c4f8c
Sachin kumar prasad <39449370+Sachin-Prasad-29@users.noreply.github.com>
2022-06-14 08:07:08 +0530

    Time: 20 ms (18.16%), Space: 47.2 MB (33.88%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
@@ -1,30 +1,21 @@
 class Solution {
-    public int minDistance(String w1, String w2) {
-        int M = w1.length();
-        int N = w2.length();
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m+1][n+1];
+        for(int[] d: dp) Arrays.fill(d,-1);
         
-        int[][] t = new int[M+1][N+1];
+        int lcs = LCS(word1,word2,m,n,dp);
         
-        // initialization
-        for(int[] a : t){
-            Arrays.fill(a,-1);
+        return m+n-(2*lcs);
+    }
+    private static int LCS(String s1,String s2,int m,int n,int[][] dp){
+        if(n==0 || m==0) return 0;
+        if(dp[m][n] != -1) return dp[m][n];
+        if(s1.charAt(m-1) == s2.charAt(n-1)){
+            return dp[m][n] = 1+LCS(s1,s2,m-1,n-1,dp);
         }
-        for(int i = 0;i < M+1; i++)
-            t[i][0] = 0;
-        for(int i = 0 ; i < N+1; i++)
-            t[0][i] = 0;
-        
-        // Iteration process
-        for(int i = 1; i < M+1; i++)
-            for(int j = 1; j < N+1; j++){
-                if(w1.charAt(i-1) == w2.charAt(j-1))
-                    t[i][j] = 1+ t[i-1][j-1];
-                else
-                    t[i][j] = Math.max(t[i-1][j],t[i][j-1]);
-            }
-        int ans = M - t[M][N];
-        ans += N- t[M][N];
-        return ans;
-        
+        else
+            return dp[m][n] = Math.max(LCS(s1,s2,m-1,n,dp),LCS(s1,s2,m,n-1,dp));
     }
 }
