7a9bc63ebdad89b29aab95b27516c810b18aa67c
Sachin kumar prasad <39449370+Sachin-Prasad-29@users.noreply.github.com>
2022-06-16 00:13:55 +0530

    Time: 1 ms (44.79%), Space: 40.8 MB (51.81%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -18,26 +18,27 @@ class Solution {
          
          return temp[n-1];
      }
-    // Buttom up (tabuluation approach)
-     public int uniquePatsdhs(int m, int n) {
-         int[][] dp = new int[m][n];
-         dp[0][0] = 1;
-         for(int i= 0; i < m; i++ )
-             for(int j = 0; j < n; j++ ){
-                 if(i == 0 && j == 0) dp[i][j]= 1;
-                 else{
-                     int left = 0, up = 0;
-                     if(j > 0) left = dp[i][j-1];
-                     if(i > 0) up  = dp[i-1][j];
-                     dp[i][j] = up+left;
-                 }
-              }
-         
-         return dp[m-1][n-1];
-     }
 }
+//     // Buttom up (tabuluation approach)
+//      public int uniquePatsdhs(int m, int n) {
+//          int[][] dp = new int[m][n];
+//          dp[0][0] = 1;
+//          for(int i= 0; i < m; i++ )
+//              for(int j = 0; j < n; j++ ){
+//                  if(i == 0 && j == 0) dp[i][j]= 1;
+//                  else{
+//                      int left = 0, up = 0;
+//                      if(j > 0) left = dp[i][j-1];
+//                      if(i > 0) up  = dp[i-1][j];
+//                      dp[i][j] = up+left;
+//                  }
+//               }
+         
+//          return dp[m-1][n-1];
+//      }
+// }
     
-    // Mempization dp 
+//     Mempization dp 
 //     static int[][] dp;
 //     public int uniquePaths(int m, int n) {
 //         dp = new int[m][n];
