58fbec581ae562d79528ba6d2983af82cf852be3
Sachin kumar prasad <39449370+Sachin-Prasad-29@users.noreply.github.com>
2022-07-06 02:01:55 +0530

    Time: 646 ms (16.23%), Space: 218.8 MB (5.02%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,29 +1,53 @@
 class Solution {
+    
     public int longestConsecutive(int[] nums) {
         if(nums.length == 0) return 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
         
+        HashSet<Integer> set = new HashSet<>();
         for(int i = 0 ; i < nums.length; i++){
-            pq.offer(nums[i]);
+            set.add(nums[i]);
         }
-        
-        int max_len = 1;
-        
-        int curr_len = 1; 
-        int ele = pq.poll();
-        while(!pq.isEmpty()){
-            int val = pq.poll();
-            if(ele == val) continue;
-            if(ele == val -1){
-                
-                curr_len++;
-                max_len = Math.max(max_len, curr_len);
-                
+        int result = 1;
+        for(int i = 0 ; i < nums.length;i++){
+            if(set.contains(nums[i]-1)) continue;
+            else{
+                int curr_seq = 1; 
+                int start = nums[i];
+                while(set.contains(start+1)){
+                    curr_seq++;
+                    start++;
+                }
+               result = Math.max(result,curr_seq); 
             }
-            else 
-                curr_len= 1;
-            ele = val;
         }
-        return max_len;
+        return result;
+        
     }
+//     public int longestConsecutive(int[] nums) {
+//         if(nums.length == 0) return 0;
+//         PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+//         for(int i = 0 ; i < nums.length; i++){
+//             pq.offer(nums[i]);
+//         }
+        
+//         int max_len = 1;
+        
+//         int curr_len = 1; 
+//         int ele = pq.poll();
+//         while(!pq.isEmpty()){
+//             int val = pq.poll();
+//             if(ele == val) continue;
+//             if(ele == val -1){
+                
+//                 curr_len++;
+//                 max_len = Math.max(max_len, curr_len);
+                
+//             }
+//             else 
+//                 curr_len= 1;
+//             ele = val;
+//         }
+//         return max_len;
+//     }
 }
