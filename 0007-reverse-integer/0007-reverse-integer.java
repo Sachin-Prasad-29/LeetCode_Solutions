796b7c08882aca2dfc5e38b715d526a834bf836e
Sachin kumar prasad <39449370+Sachin-Prasad-29@users.noreply.github.com>
2022-11-11 23:33:15 +0530

    Time: 2 ms (85.00%), Space: 41.1 MB (66.55%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -13,8 +13,8 @@ class Solution {
             val = (val*10) + (x % 10);
             x = x/10;
         }
-        System.out.println(val);
-        System.out.println(max+ " " + min);
+        // System.out.println(val);
+        // System.out.println(max+ " " + min);
         if(isNegative) val = val * -1;
         
         if(val > max || val < min) return 0;
