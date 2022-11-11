5ba3ee202b48fdf9acfb06c2ec7fcaf9244e9d87
Sachin kumar prasad <39449370+Sachin-Prasad-29@users.noreply.github.com>
2022-11-12 00:02:51 +0530

    Added solution - LeetHub

---
+++ Armstrong Numbers - GFG/armstrong-numbers.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.*;
+import java.io.*;
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+
+            System.out.println(ob.armstrongNumber(n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    static String armstrongNumber(int n){
+       String str = n+"";
+       int m = str.length();
+       int val = n;
+       int res = 0;
+       
+       while(n > 0){
+            res = res + (int)Math.pow(n%10,m);
+            n = n/10;
+       }
+      if(res == val) return "Yes";
+       
+       return "No";
+    }
+}
